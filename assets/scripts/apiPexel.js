59e65cd4233c01e3863ce5ec369fce99424e3b9d
jordanlat <40663699+jordanlat@users.noreply.github.com>
2020-05-10 23:12:08 +0200

    fix bug js

--- assets/scripts/apiPexel.js
+++ assets/scripts/apiPexel.js
@@ -1,7 +1,8 @@
 
 
 const pexelKey = "563492ad6f91700001000001bbc65744e45f4a79a6ac524672ebf124"; 
-const keyWord = "outfitoftheday";
+const keyWord = "outfit";
+// const imgDom = document.getElementsByTagName('img');
 const imgDom = document.getElementsByClassName('item__image');
 const nbrpic = imgDom.length;
 const url = "https://api.pexels.com/v1/search?query=" + keyWord + "&per_page=" + nbrpic; 
@@ -11,16 +12,21 @@ const legit = {
         'Authorization' : pexelKey
     }
 }
+console.log(imgDom);
+
 
 
 fetch(url,legit)
     .then(api => api.json())
     .then(api => {
         for( src in imgDom){
+            
             // console.log(src);
             // console.log(imgDom);
             // console.log(api.photos[src].src.original);
-            // console.log(imgDom[src]);
+            // imgDom[src].src = "lol";
+            // console.log(imgDom[src].src);
+
             imgDom[src].src = api.photos[src].src.original;
 
         }
