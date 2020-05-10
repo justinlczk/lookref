f6fd78980c38900284871c59e7eb4cd6fdf7c314
Justin <justin.luczak@hotmail.fr>
2020-05-10 22:18:57 +0200

    ajout api

---
+++ assets/scripts/apiPexel.js
@@ -0,0 +1,27 @@
+
+
+const pexelKey = "563492ad6f91700001000001bbc65744e45f4a79a6ac524672ebf124"; 
+const keyWord = "outfitoftheday";
+const imgDom = document.getElementsByClassName('item__image');
+const nbrpic = imgDom.length;
+const url = "https://api.pexels.com/v1/search?query=" + keyWord + "&per_page=" + nbrpic; 
+
+const legit = {
+    headers: {
+        'Authorization' : pexelKey
+    }
+}
+
+
+fetch(url,legit)
+    .then(api => api.json())
+    .then(api => {
+        for( src in imgDom){
+            // console.log(src);
+            // console.log(imgDom);
+            // console.log(api.photos[src].src.original);
+            // console.log(imgDom[src]);
+            imgDom[src].src = api.photos[src].src.original;
+
+        }
+    });
